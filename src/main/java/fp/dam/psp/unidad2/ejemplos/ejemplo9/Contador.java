30fd3e8102d4969e1c2958a4cfc54eb3fdbe8ba1
Julio Pedro López Bayón <jplbayon@gmail.com>
2024-10-04 19:42:04 +0200

    Actualización

---
+++ src/main/java/fp/dam/psp/unidad2/ejemplos/ejemplo9/Contador.java
@@ -0,0 +1,17 @@
+package fp.dam.psp.unidad2.ejemplos.ejemplo9;
+
+public class Contador {
+
+    private int n;
+
+    public Contador(int n) {
+        this.n = n;
+    }
+
+    public void inc() {
+        n++;
+    }
+    public int get() {
+        return n;
+    }
+}
