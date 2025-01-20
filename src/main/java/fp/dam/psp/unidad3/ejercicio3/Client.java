8bc809fdbfd469077788042bcd014e6d7095ef17
Julio Pedro López Bayón <jplbayon@gmail.com>
2025-01-20 19:40:05 +0100

    Actualización

--- src/main/java/fp/dam/psp/unidad3/ejercicio3/Client.java
+++ src/main/java/fp/dam/psp/unidad3/ejercicio3/Client.java
@@ -2,19 +2,25 @@ package fp.dam.psp.unidad3.ejercicio3;
 
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
+import java.io.EOFException;
 import java.io.IOException;
 import java.net.Socket;
+import java.time.LocalDateTime;
 
 public class Client {
 
     public static void main(String[] args) throws IOException {
-        final Socket socket = new Socket("localhost", 9000);
-        try (socket) {
+        try (Socket socket = new Socket("localhost", 9000)) {
             DataOutputStream out = new DataOutputStream(socket.getOutputStream());
             for(int i=0; i<10; i++)
                 out.writeUTF("hola " + i);
             socket.shutdownOutput();
-            for(;;);
+            DataInputStream in = new DataInputStream(socket.getInputStream());
+            try {
+                for (; ; )
+                    System.out.println(in.readUTF());
+            } catch (EOFException e) {
+            }
         }
     }
 
